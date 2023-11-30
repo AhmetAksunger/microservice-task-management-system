4694d6a676915c33de68a91fca5983d381bed1cc
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-12-01 00:36:19 +0300

    Error Handling with Resilience4j

--- task-service/src/main/java/com/taskmanagement/taskservice/client/UserServiceClient.java
+++ task-service/src/main/java/com/taskmanagement/taskservice/client/UserServiceClient.java
@@ -1,6 +1,9 @@
 package com.taskmanagement.taskservice.client;
 
 import com.taskmanagement.taskservice.dto.UserDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +14,20 @@ import java.util.List;
 @FeignClient(name = "user-service", path = "/api/v1")
 public interface UserServiceClient {
 
+    Logger logger = LoggerFactory.getLogger(UserServiceClient.class);
+
     @GetMapping("/users")
+    @CircuitBreaker(name = "getAllUsersCircuitBreaker", fallbackMethod = "getAllUsersFallback")
     ResponseEntity<List<UserDto>> getAllUsers();
+
+    /**
+     * Only to show an example of resilience-4j.
+     * @param e Exception.
+     */
+    @SuppressWarnings({"unused"})
+    default void getAllUsersFallback(Exception e) {
+        logger.info("Get All Users endpoint has failed!");
+    }
 
     @GetMapping("/users/{id}")
     ResponseEntity<UserDto> getUserById(@PathVariable String id);
