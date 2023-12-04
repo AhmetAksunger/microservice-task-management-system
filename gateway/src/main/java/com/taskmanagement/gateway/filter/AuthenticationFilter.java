5992cee898d85804d678f63ca4f42ddb209df92d
AhmetAksunger <ahmetaksungerbilgi@gmail.com>
2023-12-04 19:55:51 +0300

    Create AuthenticationFilter and filter the requests

---
+++ gateway/src/main/java/com/taskmanagement/gateway/filter/AuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.taskmanagement.gateway.filter;
+
+import com.taskmanagement.gateway.util.JwtUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    private final RouteValidator routeValidator;
+    private final JwtUtil jwtUtil;
+
+    public AuthenticationFilter(RouteValidator routeValidator, JwtUtil jwtUtil) {
+        super(Config.class);
+        this.routeValidator = routeValidator;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+
+            if (routeValidator.isSecured.test(exchange.getRequest())) {
+
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    this.onError(exchange);
+                }
+
+                try {
+                    List<String> authHeaders = Optional.ofNullable(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION))
+                            .orElseThrow();
+
+                    String bearerToken = Optional.ofNullable(authHeaders.get(0))
+                            .filter(auth -> auth.startsWith("Bearer "))
+                            .orElseThrow();
+
+                    String jwtToken = bearerToken.substring(7);
+
+                    jwtUtil.validateToken(jwtToken);
+
+                } catch (Exception e) {
+                    return onError(exchange);
+                }
+            }
+
+
+            return chain.filter(exchange);
+        });
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.setComplete();
+    }
+
+    public static class Config {
+    }
+}
